15e80a952f8d73c5438cf129069b6883c7ce7f55
helloqidi <helloqidi@gmail.com>
2015-07-28 17:54:40 +0800

    init project

---
+++ config/application.rb
@@ -0,0 +1,37 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Lawyer
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    
+    # 使用前不用require.
+    # config.autoload_paths += %W(#{config.root}/uploaders)
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    ##不使用东八区时间,而是用Rails默认的UTC时间,只是在页面显示时给予转换
+    #config.active_record.default_timezone = :local 
+    #config.time_zone = 'Beijing'
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = "zh-CN"
+
+    config.encoding = "utf-8"
+  end
+end
+I18n.config.enforce_available_locales = false
+I18n.locale = 'zh-CN'
